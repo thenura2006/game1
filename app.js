4838971356744a8a2572c0b681275fbbd7206de0
thenura2006 <117631391+thenura2006@users.noreply.github.com>
2025-04-02 20:28:00 +0530

    some changes

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
 var player = 0;
 var player_0 = [];
+var count_0 = 0;
+var count_1 = 0;
 var player_1 = [];
-var score_0 = 0;
-var score_1 = 0;
 
 function get_id(ret_id) {
   var btn = document.getElementById(ret_id);
   if (player == 0) {
     player_0.push(ret_id);
     btn.style.background = "red";
-    btn.innerHTML = ret_id;
-    btn.style.color = "white";
-    btn.style.fontSize = "20px";
     player = 1;
   } else {
     player_1.push(ret_id);
     btn.style.background = "blue";
-    btn.innerHTML = ret_id;
-    btn.style.color = "white";
-    btn.style.fontSize = "20px";
     player = 0;
   }
   btn.disabled = true;
@@ -46,13 +40,19 @@ function check_winner() {
     var check_0 = low[i].every((val) => player_0.includes(val));
     var check_1 = low[i].every((val) => player_1.includes(val));
     if (check_0) {
+      var player_0_score = document.getElementById("player_0_score");
+      count_0++;
+      player_0_score.innerHTML = count_0;
       alert("Player 1 wins!");
-      score_0++;
+      player = 0;
       reset_game();
       return;
     } else if (check_1) {
+      var player_1_score = document.getElementById("player_1_score");
+      count_1++;
+      player_1_score.innerHTML = count_1;
       alert("Player 2 wins!");
-      score_1++;
+      player = 1;
       reset_game();
       return;
     } else if (player_0.length + player_1.length == 9) {
@@ -66,7 +66,6 @@ function check_winner() {
 function reset_game() {
   player_0 = [];
   player_1 = [];
-  player = 0;
   for (var i = 1; i <= 3; i++) {
     for (var j = 1; j <= 3; j++) {
       var btn_id = i + String.fromCharCode(64 + j);
